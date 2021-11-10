73bd90ae544788d1083ff08cca85ff687453a114
Dreamlone <mik_sar@mail.ru>
2021-11-10 19:00:20 +0300

    minor refactor

--- fedot/api/api_utils/api_data.py
+++ fedot/api/api_utils/api_data.py
@@ -7,7 +7,16 @@ from fedot.core.repository.tasks import Task, TaskTypesEnum
 from fedot.core.pipelines.pipeline import Pipeline
 
 
-class ApiDataHelper:
+class ApiDataSources:
+    """
+    Class for selecting optimal data processing strategies based on type of data.
+    Available data sources are:
+        * numpy array
+        * pandas DataFrame
+        * string (path to csv file)
+        * InputData (FEDOT dataclass)
+    """
+
     def define_data(self,
                     ml_task: Task,
                     features: Union[str, np.ndarray, pd.DataFrame, InputData, dict],
