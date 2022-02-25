710a83f903a4fbedfb483374f67504c4c4d0f3ab
Dreamlone <mik_sar@mail.ru>
2022-02-25 16:33:07 +0300

    time check

--- cases/errors/launch_fedot.py
+++ cases/errors/launch_fedot.py
@@ -1,3 +1,5 @@
+import timeit
+
 import pandas as pd
 import numpy as np
 
@@ -19,12 +21,16 @@ def run_classification_example(dataset_name: str, timeout: float = None):
         target = np.array(df['Class'])
     else:
         raise ValueError()
+    start_time = timeit.default_timer()
 
-    model = Fedot(problem='classification', timeout=timeout, verbose_level=1)
+    model = Fedot(problem='classification', timeout=timeout, verbose_level=4,
+                  composer_params={'pop_size': 2})
     model.fit(features=features, target=target)
 
     model.predict(features)
+    computation_time = timeit.default_timer() - start_time
+    print(f'Computation time: {computation_time}')
 
 
 if __name__ == '__main__':
-    run_classification_example(dataset_name='volkert.csv', timeout=5)
+    run_classification_example(dataset_name='cnae-9.csv', timeout=1)
