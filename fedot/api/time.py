85be907a8be19b2fa7976dba8679ae67fa39ef10
Sergei Pakulin <pakylin3@mail.ru>
2022-03-25 17:01:32 +0500

    ApiTime hotfix

--- fedot/api/time.py
+++ fedot/api/time.py
@@ -1,4 +1,5 @@
 import datetime
+
 from contextlib import contextmanager
 
 from fedot.core.constants import COMPOSING_TUNING_PROPORTION
@@ -60,9 +61,9 @@ class ApiTime:
         :param init_pipeline_fit_time: time to fit stand-alone pipeline
         """
         all_spended_time = self.composing_spend_time + init_pipeline_fit_time
-        all_timeout = float(self.time_for_automl)
 
         if self.time_for_automl is not None:
+            all_timeout = float(self.time_for_automl)
             timeout_in_sec = datetime.timedelta(minutes=all_timeout).total_seconds()
             timeout_for_tuning = timeout_in_sec - all_spended_time.total_seconds()
         else:
